4ccb86dc0b4071c9247b4c295bce1fa6797019d6
Li <limingyialpha@gmail.com>
2021-11-22 12:10:59 +0100

    correct zinv implementation

--- generators/zinv.py
+++ generators/zinv.py
@@ -6,6 +6,9 @@ class Zinv(DataGenerator):
     name = "zinv"
 
     def get_points(self, n: int) -> np.ndarray:
+        def add_uniform_noise(x: int, noise_value: float):
+            return x + np.random.uniform(-noise_value/2, noise_value/2)
+
         def get_next_dim_value(head):
             toss = np.random.randint(3)
             value = 0
@@ -19,5 +22,6 @@ class Zinv(DataGenerator):
 
         def get_point():
             pivot = np.random.random()
-            return np.array([get_next_dim_value(pivot) if (i >= 2) else pivot for i in range(1, self.dim+1)])
+            no_noise = np.array([get_next_dim_value(pivot) if (i >= 2) else pivot for i in range(1, self.dim+1)])
+            return np.array([add_uniform_noise(x, self.noise) for x in no_noise])
         return np.array([get_point() for _ in range(n)])
