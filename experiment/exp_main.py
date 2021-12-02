fd6af848885dcd1821e83641bf89ab8fc5a01331
Li <limingyialpha@gmail.com>
2021-12-02 12:00:25 +0100

    put mian outside

--- experiment/exp_main.py
+++
@@ -1,6 +0,0 @@
-from experiment import canonical_correlation_power_compare
-
-
-if __name__ == '__main__':
-    exp_instance = canonical_correlation_power_compare.CanonicalCorrelationPowerCompare()
-    exp_instance.run()
--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import dcor
-import numpy as np
-
-
-def main():
-    x = np.array([[1, 1], [2, 2]])
-    y = np.array([[1, 3], [2, 4]])
-    print(dcor.independence.distance_correlation_t_test(x,y).p_value)
+from experiment import canonical_correlation_power_compare
 
 
 if __name__ == '__main__':
-    main()
+    cc = canonical_correlation_power_compare.CanonicalCorrelationPowerCompare()
+    cc.run()
 
