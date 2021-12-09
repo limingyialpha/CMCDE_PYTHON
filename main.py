3688492c6ee9e151298f5079b453d261c058641f
Li <limingyialpha@gmail.com>
2021-12-09 15:42:33 +0100

    edit main

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from experiment.power import GC3PowerM
+from experiment.scalability import GC3ScalabilityD, GC3ScalabilityO
 
 if __name__ == '__main__':
+    GC3ScalabilityO.GC3ScalabilityO().run()
+    GC3ScalabilityD.GC3ScalabilityD().run()
     GC3PowerM.GC3PowerM().run()
+
