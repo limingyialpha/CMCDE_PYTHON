9569a50e97a603bb72525055e9775634fde81f4a
Li <limingyialpha@gmail.com>
2022-01-20 18:22:37 +0100

    minor

--- experiment/factory.py
+++ experiment/factory.py
@@ -1,6 +1,5 @@
 import logging
 import sys
-from typing import List
 
 from experiment.dependencies.Dependencies import Dependencies
 from experiment.power.CCPowerM import CCPowerM
@@ -25,7 +24,7 @@ experiments_dictionary = {
 }
 
 
-def run(experiments: List[str], output_folder: str):
+def run(experiments, output_folder: str):
     if len(experiments) == 0:
         raise RuntimeError('No experiments given!')
     elif len(experiments) == 1 and experiments[0] == "all":
